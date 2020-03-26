bfc2d2f87cb0838de5e66ed05a4bf4fca77c4802
Adebo Timothy <57668303+TimTyger@users.noreply.github.com>
2020-03-26 08:12:39 +0100

    Commit

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,64 +10,26 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-           // List<string> value = new List<string>();
-
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
-            //value.Add(name);
-            Console.WriteLine("What is your height in cm?");
-            var height = Console.ReadLine();
-            //value.Add(height);
-            Console.WriteLine("What is your best food?");
-            var bestFood = Console.ReadLine();
-            //value.Add(bestFood);
-            Console.WriteLine("What is your age?");
-            var age = Console.ReadLine();
-            //value.Add(age);
-            Console.WriteLine("What football club do you support");
-            var footBallClub = Console.ReadLine();
-            //value.Add(footBallClub);
-            Console.WriteLine("What is your hobby?");
-            var hobby = Console.ReadLine();
-            //value.Add(hobby);
-            Console.WriteLine("What is your complexion?");
-            var complexion = Console.ReadLine();
-            //value.Add(complexion);
-
-            Arinze a = new Arinze();
-            {
-                a.age = Int32.Parse(age);
-                a.bestFood = bestFood;
-                a.complexion = complexion;
-                a.footBallClub = footBallClub;
-                a.Height = Int32.Parse(height);
-                a.Hobby = hobby;
-                a.Name = name;
-            }
-
-            if (footBallClub == "Manchester United")
-            {
-                Console.WriteLine($"Welcome {name}, you are {age} years old, {height}cm tall, and {complexion} in complexion. Your best food is {bestFood}, you love {hobby} and you support {footBallClub} football club.");
-            }
-            else
-            {
-                Console.Write("ERROR!!!, Stop supporting inferior clubs");
-            }
-
-            //foreach (var b in value)
-            //{
-            //    Console.WriteLine(b);
-            //}
-            //Console.ReadKey();
-
-            //Array value = new Array();
-            // string[] array =  value.tim;
-            //foreach (var a in array)
-            //{
-            //    Console.WriteLine($"{a} is here");
-            //}
-            //Console.ReadKey();
-
+           /Console.WriteLine("Please Insert your card");
+       Console.ReadLine();
+       Console.WriteLine("Enter your 4 digit pin");
+       var pin = Int32.Parse(Console.ReadLine());
+       var regPin = 8888;
+       if (pin == regPin)
+       {
+         
+       
+       Console.WriteLine("Enter account number");
+       var acct = Console.ReadLine();
+       if (acct.Length != 10)
+       {
+         Console.WriteLine("Incorrect password");
+       }
+       }
+       else
+       {
+         Console.WriteLine("Incorrect pin");
+       }
         }
     }
 }
